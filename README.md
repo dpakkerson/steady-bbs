7b9add12c3f69e1b807694e69466731a4b12b8b8
dpakkerson <104380309+dpakkerson@users.noreply.github.com>
2022-05-21 22:42:59 +0900

    added readme

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Steady BBS
 
 2ch ブラウザ互換掲示板サーバー
+
+
+## 設置
+
+### 前提
+
+Node.js と npm と npx が動作すること
+
+### 共通準備
+
+
+#### ライブラリをインストール
+
+serverとclientでnpm installします
+```sh
+cd server && npm install
+cd ..
+cd client && npm install
+```
+
+#### データベース初期化
+
+serverでprismaの初期化を行います
+```sh
+cd server
+npx prisma db push
+npx prisma generate
+```
+
+### 開発用
+
+serverとclientでそれぞれnpm run devとnpm startを実行します
+```sh
+cd server && npm run dev &
+cd ..
+cd client && npm start &
+```
+
+localhost:3000にアクセスすると掲示板が見えます
+
+### サーバーにデプロイする
+
+1. フロントエンドのコードをビルドします
+```sh
+cd client && npm build
+cd ..
+```
+
+2. `NODE_ENV`を`production`に、`STEADYBBS_SERVE_FRONTEND`に何かしらの値をセットしてサーバーを起動します
+```sh
+cd server
+NODE_ENV=production STEADYBBS_SERVE_FRONTEND=1 npm run dev
+```
+
+`PORT`環境変数でポート番号を変更できます。
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,23 @@
+import path from 'path';
 import express from "express";
 import { app as webapp } from "./controllers/web";
 import { app as restapp } from "./controllers/rest";
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use("/", webapp);
 app.use("/api", restapp);
 
+if (process.env.NODE_ENV === 'production') {
+    if (process.env.STEADYBBS_SERVE_FRONTEND) {
+        const frontendRoot = path.join(__dirname, '../../client/build');
+        app.use(express.static(frontendRoot));
+        app.get("/*", (req, res) => {
+            res.sendFile(path.join(frontendRoot, 'index.html'));
+        });        
+    }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const server = app.listen(3001);
+const server = app.listen(PORT, () => console.log(`Steady BBS Server running on Port ${PORT}`));
